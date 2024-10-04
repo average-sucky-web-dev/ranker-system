6de910f5a938a15b7064426d6b3b19b937b3bfc8
average-sucky-web-dev <144091957+average-sucky-web-dev@users.noreply.github.com>
2024-10-04 12:45:20 +1000

    Update server.js
    bugfix

--- Ranking Service/server.js
+++ Ranking Service/server.js
@@ -1,24 +1,27 @@
 const express = require('express')
 require('dotenv').config({'path': __dirname + '/Secrets.env'})
 const axios = require('axios')
+axios.defaults.validateStatus = function() {
+    return true
+}
 const app = express()
-const cookie = process.env.cookie
+const cookie = process.env.COOKIE
 app.use(express.json())
 app.get('/groupdata', async (req, res) => {
     const table = {}
     const response = await axios.get('https://groups.roblox.com/v1/groups/12102956/roles')
-    for (const value of list) {
+    for (const value of response.data.roles) {
         if (value.name === "Guest") continue
         table[value.name] = value.id
     }
     return res.send(table)
 })
 app.post('/rank', async (req, res) => {
-    const data = req.data
+    const data = req.body
     if (!data || data.secret !== process.env.SECRET) return res.sendStatus(403)
-    const response = await axios.patch('https://groups.roblox.com/v1/groups/12102956/users/' + req.data.ranked,
+    const response = await axios.patch('https://groups.roblox.com/v1/groups/12102956/users/' + data.ranked,
         {
-            'roleId': req.data.role
+            'roleId': data.role
         },
         {
           headers: {
@@ -26,10 +29,13 @@ app.post('/rank', async (req, res) => {
           }
         }
     )
-    if (response.status !== 403) res.sendStatus(500)
-    const response2 = await axios.patch('https://groups.roblox.com/v1/groups/12102956/users/' + req.data.ranked,
+    if (response.status !== 403) {
+        console.log(response.status)
+        return res.sendStatus(500)
+    }
+    const response2 = await axios.patch('https://groups.roblox.com/v1/groups/12102956/users/' + data.ranked,
         {
-            'roleId': req.data.role
+            'roleId': data.role
         },
         {
           headers: {
@@ -38,10 +44,17 @@ app.post('/rank', async (req, res) => {
           }
         }
     )
-    if (response.status !== 200) res.sendStatus(500)
+    if (response2.status !== 200) {
+        console.log('failed at second')
+        console.log(response2.status)
+        console.log(response2.data)
+        return res.sendStatus(500)
+    }
     res.sendStatus(200)
     await axios.post(process.env.WEBHOOKURL, {
         content: `User **${data.ranker}** ranked **${data.rankedname}** from **${data.oldrank}** to **${data.newrank}**`
     })
 })
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('systems running')
+})
